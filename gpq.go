7278246165ce66633bb88bbbf165e4f84ff6f56d
Tiger Wang <tigerwang@outlook.com>
2023-09-22 09:16:07 +0800

    init commit (#1)

---
+++ gpq.go
@@ -0,0 +1,53 @@
+package gpq
+
+import (
+	"container/heap"
+)
+
+type PriorityQueue[T any] struct {
+	queue []*T
+	less  func(i, j *T) bool
+}
+
+func (pq PriorityQueue[T]) Len() int {
+	return len(pq.queue)
+}
+
+func (pq PriorityQueue[T]) Less(i, j int) bool {
+	return pq.less((pq.queue)[i], (pq.queue)[j])
+}
+
+func (pq *PriorityQueue[T]) Pop() any {
+	if pq.Len() == 0 {
+		return nil
+	}
+
+	item := (pq.queue)[pq.Len()-1]
+
+	pq.queue = (pq.queue)[:pq.Len()-1]
+
+	return item
+}
+
+func (pq *PriorityQueue[T]) Push(item any) {
+	_item := item.(T)
+	pq.queue = append(pq.queue, &_item)
+}
+
+func (pq *PriorityQueue[T]) Swap(i, j int) {
+	if i >= pq.Len() || j >= pq.Len() {
+		return
+	}
+	(pq.queue)[i], (pq.queue)[j] = (pq.queue)[j], (pq.queue)[i]
+}
+
+func NewPriorityQueue[T any](less func(i, j *T) bool) PriorityQueue[T] {
+	pq := PriorityQueue[T]{
+		queue: make([]*T, 0),
+		less:  less,
+	}
+
+	heap.Init(&pq)
+
+	return pq
+}
---
+++ gpq_test.go
@@ -0,0 +1,54 @@
+package gpq_test
+
+import (
+	"container/heap"
+	"testing"
+
+	"github.com/tigerinus/gpq"
+)
+
+type Expirable[T any] struct {
+	Data           T
+	ExpirationTime int64
+}
+
+func TestPriorityQueue(t *testing.T) {
+	pq := gpq.NewPriorityQueue[Expirable[string]](func(i, j *Expirable[string]) bool {
+		return i.ExpirationTime > j.ExpirationTime
+	})
+
+	e1 := Expirable[string]{
+		Data:           "test1",
+		ExpirationTime: 1,
+	}
+
+	e2 := Expirable[string]{
+		Data:           "test2",
+		ExpirationTime: 2,
+	}
+
+	e3 := Expirable[string]{
+		Data:           "test3",
+		ExpirationTime: 3,
+	}
+
+	heap.Push(&pq, e2)
+	heap.Push(&pq, e1)
+	heap.Push(&pq, e3)
+
+	for i := 3; i > 0; i-- {
+		if i != pq.Len() {
+			t.Fail()
+		}
+
+		item := heap.Pop(&pq)
+		e, ok := item.(*Expirable[string])
+		if !ok {
+			t.Fail()
+		}
+
+		if e.ExpirationTime != int64(i) {
+			t.Fail()
+		}
+	}
+}
