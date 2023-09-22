829ef7b60f11750697973c55af75961e9244634e
Tiger Wang <tigerwang@outlook.com>
2023-09-21 22:03:20 -0400

    wip

--- gpq.go
+++ gpq.go
@@ -10,8 +10,17 @@ type PriorityQueue[T any] struct {
 	ipq internal.PriorityQueue[T]
 }
 
-func (pq PriorityQueue[T]) Push(item *T) {
+func (pq PriorityQueue[T]) Len() int {
+	return pq.ipq.Len()
+}
+
+func (pq *PriorityQueue[T]) Push(item T) {
 	heap.Push(&pq.ipq, item)
+}
+
+func (pq *PriorityQueue[T]) Pop() T {
+	item := heap.Pop(&pq.ipq)
+	return item.(T)
 }
 
 func NewPriorityQueue[T any](less func(i, j T) bool) PriorityQueue[T] {
---
+++ gpq_test.go
@@ -0,0 +1,49 @@
+package gpq_test
+
+import (
+	"testing"
+
+	"github.com/tigerinus/gpq"
+)
+
+type Expirable[T any] struct {
+	Data           T
+	ExpirationTime int64
+}
+
+func TestPriorityQueue(t *testing.T) {
+	pq := gpq.NewPriorityQueue[*Expirable[string]](func(i, j *Expirable[string]) bool {
+		return i.ExpirationTime > j.ExpirationTime
+	})
+
+	e1 := Expirable[string]{
+		Data:           "test1",
+		ExpirationTime: 1,
+	}
+
+	e2 := Expirable[string]{
+		Data:           "test2",
+		ExpirationTime: 2,
+	}
+
+	e3 := Expirable[string]{
+		Data:           "test3",
+		ExpirationTime: 3,
+	}
+
+	pq.Push(&e2)
+	pq.Push(&e1)
+	pq.Push(&e3)
+
+	for i := 3; i > 0; i-- {
+		if i != pq.Len() {
+			t.Fail()
+		}
+
+		e := pq.Pop()
+
+		if e.ExpirationTime != int64(i) {
+			t.Fail()
+		}
+	}
+}
