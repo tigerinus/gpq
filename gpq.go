86d986cdeabf0108cdae311e1422de60568e1b96
Tiger Wang <tigerwang@outlook.com>
2023-09-24 16:52:51 +0800

    fix panic when `peek()` an `pop()` reach to the bottom of the queue (#5)

--- gpq.go
+++ gpq.go
@@ -14,18 +14,30 @@ func (pq PriorityQueue[T]) Len() int {
 	return pq.ipq.Len()
 }
 
-func (pq PriorityQueue[T]) Peek() T {
+func (pq PriorityQueue[T]) Peek() *T {
 	item := pq.ipq.Peek()
-	return item.(T)
+
+	if item == nil {
+		return nil
+	}
+
+	typed := item.(T)
+	return &typed
 }
 
 func (pq *PriorityQueue[T]) Push(item T) {
 	heap.Push(&pq.ipq, item)
 }
 
-func (pq *PriorityQueue[T]) Pop() T {
+func (pq *PriorityQueue[T]) Pop() *T {
 	item := heap.Pop(&pq.ipq)
-	return item.(T)
+
+	if item == nil {
+		return nil
+	}
+
+	typed := item.(T)
+	return &typed
 }
 
 func NewPriorityQueue[T any](less func(i, j T) bool) PriorityQueue[T] {
